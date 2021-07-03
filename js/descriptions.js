369fc094a3167bd2161f500f5242f146b82ff224
Rudolfo Torres <rudolfotorres@gmail.com>
2021-07-02 23:37:30 -0300

    adicionando js + jquery

---
+++ js/descriptions.js
@@ -0,0 +1,29 @@
+function changeDescriptions(text,width,display){
+    console.log(text);
+    console.log(width);
+    console.log(display);
+    document.querySelector(".descriptions").innerHTML = "<p>"+text+"</p>";
+};
+
+const descriptions = [
+    {
+        text: "//Programador PHP e MySQL (Back-end);",
+        width: "14.1em",
+        display: "none"
+    },
+    {
+        text: "//Desenvolvedor Web CSS, HTML5 e Javascript",
+        width: "14.1em",
+        display: "none"
+    },
+    {
+        text: "//Biblioteca 3D render (Three.js)",
+        width: "14.1em",
+        display: "none"
+    }
+];
+
+for (var i = 0; i < descriptions.length; i++) {
+    changeDescriptions(descriptions[i].text, descriptions[i].width, descriptions[i].display);
+};
+
